519d56ea1d394347fc131782c6be1fafad0309b7
Vincent <vlnguyen@unomaha.edu>
2022-09-24 02:19:06 -0700

    rename module files

--- api/src/api.module.ts
+++ api/src/api.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { SessionModule } from './modules/SessionModule';
-import { UserModule } from './modules/UserModule';
-import { AppModule } from './modules/AppModule';
-import { PostModule } from './modules/PostModule';
+import { SessionModule } from './modules/Session.module';
+import { UserModule } from './modules/User.module';
+import { AppModule } from './modules/App.module';
+import { PostModule } from './modules/Post.module';
 
 @Module({
   imports: [
