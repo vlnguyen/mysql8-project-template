7f0c2ba8acbc87c7403b730ba86e84ba8e5c51a1
Vincent <vlnguyen@unomaha.edu>
2022-09-24 00:11:14 -0700

    update HTTP methods on SessionController

--- api/src/controllers/session/session.controller.ts
+++ api/src/controllers/session/session.controller.ts
@@ -1,27 +1,20 @@
-import { Controller, Get, Session } from '@nestjs/common';
+import { Controller, Get, Post, Session } from '@nestjs/common';
 import { ISessionData } from 'src/infrastructure/types/session.types';
 
 @Controller('session')
 export class SessionController {
-  constructor() {}
-
   @Get()
-  async getRoot(): Promise<any> {
-    return '/api/session';
+  getMe(@Session() session: ISessionData) {
+    return {
+      visits: session.visits ?? null,
+    };
   }
 
-  @Get('setMe')
+  @Post()
   setMe(@Session() session: ISessionData) {
     const { visits } = session;
     const newVisits = visits ? visits + 1 : 1;
     session.visits = newVisits;
     return { newVisits };
-  }
-
-  @Get('getMe')
-  getMe(@Session() session: ISessionData) {
-    return {
-      visits: session.visits ?? null,
-    };
   }
 }
