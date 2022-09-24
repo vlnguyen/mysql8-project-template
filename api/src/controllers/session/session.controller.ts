95dc0c7bbd3b39f2fecbeeeed9bbfdd143c7cbe4
Vincent <vlnguyen@unomaha.edu>
2022-09-23 22:32:18 -0700

    initialize express-session (local memory)

--- api/src/controllers/session/session.controller.ts
+++ api/src/controllers/session/session.controller.ts
@@ -1,11 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Session } from '@nestjs/common';
 
 @Controller('session')
 export class SessionController {
   constructor() {}
 
   @Get()
-  async getUser(): Promise<any> {
+  async getRoot(): Promise<any> {
     return '/api/session';
+  }
+
+  @Get('setMe')
+  setMe(@Session() session: Record<string, any>) {
+    session.visits = session.visits ? session.visits + 1 : 1;
+    return { message: 'Set new value for session.visits' };
+  }
+
+  @Get('getMe')
+  getMe(@Session() session: Record<string, any>) {
+    return session;
   }
 }
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,23 @@
+import * as session from 'express-session';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  app.use(
+    session({
+      secret: 'sesionsecret',
+      resave: false,
+      saveUninitialized: true,
+      name: 'connect.sid',
+      cookie: {
+        maxAge: 3600000, // 1 hour
+      },
+    }),
+  );
+
   await app.listen(8080);
 }
 bootstrap();
