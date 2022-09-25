cc815fe44a02533a4c36589fce0976cee7ee867f
Vincent <vlnguyen@unomaha.edu>
2022-09-24 19:24:40 -0700

    get "Hello, World!" message from /api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useState } from "react";
+import Axios from "axios";
+import "./App.css";
 
 function App() {
+  const [helloWorld, setHelloWorld] = useState("");
+
+  const loadHelloWorld = useCallback(async () => {
+    const message = await Axios.request({ url: "/api" }).then(
+      (resp) => resp.data as string
+    );
+    setHelloWorld(message);
+  }, []);
+
+  useEffect(() => {
+    loadHelloWorld();
+  }, [loadHelloWorld]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{helloWorld}</p>
       </header>
     </div>
   );
